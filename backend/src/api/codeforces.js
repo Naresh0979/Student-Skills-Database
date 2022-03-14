1eabcd7ff29ce1b16c71e07ba4b07749e5403342
Amit Gupta <19ucs011@lnmiit.ac.in>
2022-03-14 21:46:46 +0530

    Atlas

--- backend/src/api/codeforces.js
+++ backend/src/api/codeforces.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 async function run(){
     console.log("entered");
-    const url = "https://codeforces.com/api/problemset.problems";
-    const data = await axios.get(url);
+    const url = "https://codeforces.com/api/user.rating?handle=amitgupta20";
+    const responce = await axios.get(url);
     console.log("entered");
-    console.log(data.data.result.problems[0]);
+    console.log(responce.data);
 }
+module.exports = {run};
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,10 @@ require('dotenv').config()
 const userRouter = require('./routes/users');
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
-
 const app = express(); 
 const PORT = process.env.PORT || 2000;
 
 app.use(cookieParser());
-
 app.use(express.json());
 app.use(express.urlencoded({
     extended:false 
@@ -34,4 +31,5 @@ app.use('/users',userRouter);
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 });
+
  
