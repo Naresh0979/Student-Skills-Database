0623846f344f0af78b421fee858b4964a6f91f9e
Naresh0979 <nareshkumawat0979@gmail.com>
2022-03-14 23:31:41 +0530

    profile updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import "./styles/profile.css";
 import img from "./03.jpg";
 const Profile = (props) => {
+
+  console.log(props.user);
   return (
     <div id="services" className="text-center">
       <div className="container">
-        <div className="col-md-6 col-lg-6 col-sm-12">
+        {/* <div className="col-lg-12 col-md-12 col-sm-12"> */}
+        <div className="col-lg-4 col-md-4 col-sm-12 divLeft">
           <img src={img} className="cardImg" alt="Nahi aayi" />
+          <span className="caption"><h1> {props.user.fullName}</h1></span> 
+          <button>Edit</button>
+        
         </div>
+        <div className = "userdetail">
+          
+       <span className="caption">Email : {props.user.email} </span>
+       <span className="caption"> Rating : 1600</span>
+       <span className="caption">Linkedin : nareshjsnds</span>
+       <span className="caption"> Github : nareshjsnds</span>
+       <span className="caption"> Branch : CSE</span>
+        
+        </div>
+        
+        {/* </div> */}
+       
       </div>
     </div>
   );
