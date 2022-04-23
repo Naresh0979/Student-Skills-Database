ff31afaa0ad0b7a470ee3982e047bd0809b8fa4e
Naresh0979 <nareshkumawat0979@gmail.com>
2022-04-23 11:52:37 +0530

    coding profile added

--- frontend/src/components/dashboard/CodingPlatformProfile.jsx
+++ frontend/src/components/dashboard/CodingPlatformProfile.jsx
@@ -1,18 +1,23 @@
 import { Navbar } from "../../Navbar";
 //import Card from "react-bootstrap/Card";
 import "./codingplatform.css";
-
+import RatingMeter from "../animations/RatingMeter";
 import React from "react";
 //import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
-
+import useState from 'react';
 const CodingPlatformProfile = () => {
+  // let [username,setuserName]=useState();
   return (
-    <div id='team' className='text-center'>
+    <div id="team" className="text-center">
       <div className="container">
         {/* <Navbar /> */}
-        <h1 className="heading">Coding Profiles</h1>
+        {/* <h1 className="heading">Coding Profiles</h1> */}
+        <div className='section-title'>
+          <h2>Coding Profiles</h2>
+          
+        </div>
         <div className="row">
-          <div className="col-sm-4">
+          <div className="col-sm-4 box" >
             <div className="card">
               {/* <div className="image">
                 <img src="http://loremflickr.com/320/150" />
@@ -24,41 +29,24 @@ const CodingPlatformProfile = () => {
                     alt="codeforces"
                   />
                   {/* <h2>Codeforces</h2>   */}
-
-                  <h3>username</h3>
-                  <h3>MaxRating</h3>
-                  <h3>Total Question Solved </h3>
-                </div>
-                <div className="content">
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="card">
-              {/* <div className="image">
-                <img src="http://loremflickr.com/320/150" />
-              </div> */}
-              <div className="card-inner">
-                <div className="header">
-                  <img
-                    src={require("../../assets/images/codeforces.jpg")}
-                    alt="codeforces"
-                  />
-                  {/* <h2>Codeforces</h2>   */}
-
-                  <h3>username</h3>
-                  <h3>MaxRating</h3>
-                  <h3>Total Question Solved </h3>
-                </div>
-                <div className="content">
                  
+                <div className="content">
+              
+                  <div className="content2">
+                    <span >username      - <b>naresh_2000</b><br></br></span>
+                    <span >Max Rating-<b>1200</b> <br></br></span>
+                    <span >Question Solved -<b>200</b>  </span>
+                  </div>{" "}
+                  <div className="ratingmeter">
+                    <RatingMeter />
+                    <span className="details">Rating-<b>1200</b></span>
+                  </div>{" "}
+                </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-sm-4">
+          <div className="col-sm-4 box" >
             <div className="card">
               {/* <div className="image">
                 <img src="http://loremflickr.com/320/150" />
@@ -70,17 +58,54 @@ const CodingPlatformProfile = () => {
                     alt="codeforces"
                   />
                   {/* <h2>Codeforces</h2>   */}
-
-                  <h3>username</h3>
-                  <h3>MaxRating</h3>
-                  <h3>Total Question Solved </h3>
-                </div>
+                 
                 <div className="content">
-                  
+              
+                  <div className="content2">
+                    <span >username      - <b>naresh_2000</b><br></br></span>
+                    <span >Max Rating-<b>1200</b> <br></br></span>
+                    <span >Question Solved -<b>200</b>  </span>
+                  </div>{" "}
+                  <div className="ratingmeter">
+                    <RatingMeter />
+                    <span className="details">Rating-<b>1200</b></span>
+                  </div>{" "}
+                </div>
                 </div>
               </div>
             </div>
           </div>
+          <div className="col-sm-4 box" >
+            <div className="card">
+              {/* <div className="image">
+                <img src="http://loremflickr.com/320/150" />
+              </div> */}
+              <div className="card-inner">
+                <div className="header">
+                  <img
+                    src={require("../../assets/images/codeforces.jpg")}
+                    alt="codeforces"
+                  />
+                  {/* <h2>Codeforces</h2>   */}
+                 
+                <div className="content">
+              
+                  <div className="content2">
+                    <span >username      - <b>naresh_2000</b><br></br></span>
+                    <span >Max Rating-<b>1200</b> <br></br></span>
+                    <span >Question Solved -<b>200</b>  </span>
+                  </div>{" "}
+                  <div className="ratingmeter">
+                    <RatingMeter />
+                    <span className="details">Rating-<b>1200</b></span>
+                  </div>{" "}
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          
         </div>
       </div>
     </div>
