2a21d599e3831b76c20c014c75fc8a29cd49ee97
Amit Gupta <19ucs011@lnmiit.ac.in>
2022-06-20 14:04:03 +0530

    posts

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,26 +1,21 @@
 const mongoose = require("mongoose");
 
 const post = new mongoose.Schema({
-    email: {
-        type:String,
-        
-        required:true     
-    },
-    author: {
-        type: String,
-    },
-   
-    pid: {
-        
-        type: String,
-    },
-    content: { 
-        type: String,
-    },
-    commentId: {
-        type: Array,
-    },
-
-})
-const Post = new mongoose.model("Post",post);
-module.exports = Post; 
+  email: {
+    type: String,
+    required: true,
+  },
+  pId: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+  time: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  }
+});
+const Post = new mongoose.model("Post", post);
+module.exports = Post;
--- frontend/src/components/dashboard/DoubtBlogs.jsx
+++ frontend/src/components/dashboard/DoubtBlogs.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./DoubtBlog.css";
 import { Navbar } from "../../Navbar";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 const DoubtBlogs = () => {
   let location = useLocation();
   //console.log(location);
   const [replybox, setReplybox] = useState(false);
+  const [replyPostIndex, setReplyPostIndex] = useState();
+  const [pId ,setPId] = useState(null);
+  const [replyMsg, setReplyMsg] = useState("");
   const [contentPost, setContentPost] = useState("");
   const [posts, setPosts] = useState();
+  const [seenPostIndex,setSeenPostIdx] = useState(-1);
+  // const [comment]
 
   const handlePost = (e) => {
     setContentPost(e.target.value);
@@ -18,23 +23,48 @@ const DoubtBlogs = () => {
   const handleSubmit = () => {
     let post = {
       email: location.state.email,
-      author: location.state.fullName,
       content: contentPost,
     };
 
     //console.log(post);
     axios
-      .post("http://localhost:2000/student/createpost", {
+      .post("http://localhost:2000/student/createPost", {
         post,
       })
       .then(({ data }) => {
-         console.log("Inside DATA ",data);
+        console.log("Inside DATA ", data);
         // let temporaryInfo = {
         //   name: data.name,
         //   content: data.content,
         //   comments: data.comments,
         // };
-       
+
+        posts.push(data);
+        console.log(posts);
+        setPosts(posts);
+      });
+  };
+
+  const handleCommentSubmit = () => {
+    let comment = {
+      email: location.state.email,
+      content: replyMsg,
+      pId : pId
+    };
+
+    //console.log(post);
+    axios
+      .post("http://localhost:2000/student/createComment", {
+        comment,
+      })
+      .then(({ data }) => {
+        console.log("Inside DATA ", data);
+        // let temporaryInfo = {
+        //   name: data.name,
+        //   content: data.content,
+        //   comments: data.comments,
+        // };
+
         posts.push(data);
         console.log(posts);
         setPosts(posts);
@@ -42,21 +72,22 @@ const DoubtBlogs = () => {
   };
 
   useEffect(() => {
-    axios.post("http://localhost:2000/student/getallPost",{ email:location.state.email}).then((response) => {
-    //   console.log(response);
-    //console.log(response.data);
+    axios
+      .post("http://localhost:2000/student/getallPost", {
+        email: location.state.email,
+      })
+      .then((response) => {
+        //   console.log(response);
+        //console.log(response.data);
 
-    // let temporaryInfo = {
-    //   name: response.data.name,
-    //   content: response.data.content,
-    //   comments:response.data.comments,
-    // };
-    setPosts(response.data);
-   
-   
-  });
-    
-   }, [posts])
+        // let temporaryInfo = {
+        //   name: response.data.name,
+        //   content: response.data.content,
+        //   comments:response.data.comments,
+        // };
+        setPosts(response.data);
+      });
+  }, [posts]);
 
   return (
     <div id="profileContainer">
@@ -78,7 +109,7 @@ const DoubtBlogs = () => {
                   {worklink.linkName}
                 </a>
                 <br /> */}
-                <details open className="comment" id="comment-1"  key={index} >
+                <details open className="comment" id="comment-1" key={index}>
                   <a href="#comment-1" className="comment-border-link">
                     <span className="sr-only">Jump to comment-1</span>
                   </a>
@@ -94,29 +125,37 @@ const DoubtBlogs = () => {
                           <span className="sr-only">Vote down</span>
                         </button>
                       </div> */}
+
                       <div className="comment-info">
+                        
                         <a href="#" className="comment-author">
-                          {post.author}
+                          {post.email}
                         </a>
+                        <button type="button" onClick={() => {setSeenPostIdx(index)} }> Show Post</button>
                         <p className="m-0">22 points &bull; 4 days ago</p>
+                        
                       </div>
                     </div>
                   </summary>
 
-                  <div className="comment-body">
-                    <p>
-                     {post.content}
-                    </p>
+                  {/* { seenPostIndex === index && */}
+                  
+                    <div className="comment-body">
+                    <p>{post.content}</p>
                     <button
                       type="button"
                       data-toggle="reply-form"
                       data-target="comment-1-reply-form"
-                      onClick={() => setReplybox(!replybox)}
+                      onClick={() => {
+                        setReplybox(!replybox);
+                        setReplyPostIndex(index);
+                        setPId(post.pId);
+                      }}
                     >
                       Reply
                     </button>
 
-                    {replybox ? (
+                    {replybox && index === replyPostIndex ? (
                       <form
                         method="POST"
                         className="reply-form "
@@ -125,6 +164,8 @@ const DoubtBlogs = () => {
                         <textarea
                           placeholder="Reply to comment"
                           rows="4"
+                          onChange={setReplyMsg}
+                          
                         ></textarea>
                         <button type="submit">Submit</button>
                         <button
